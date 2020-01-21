1783b2510bf0381ffc5076c15cade0fa8c93dc88
garoso <david.fd.251@gmail.com>
2020-01-21 00:29:45 -0500

    Do create method for movies and show movies in index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,22 @@
 class MoviesController < ApplicationController
+
   def index
+    @movie = Movie.new
     @movies = Movie.all
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render json: { errors: @movie.errors }, status: 422
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :description, :image_url, :days)
   end
 end
